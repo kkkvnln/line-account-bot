c86fd60fa08110daad0251622df6d1842619ac05
kkkvnln <l764651816@gmail.com>
2026-05-16 10:59:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 你傳什麼，機器人就回什麼
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
